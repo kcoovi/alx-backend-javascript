b902473fb2413740f89f048480ec2af562542d8e
Kevin COOVI <k.coovi@gmail.com>
2024-02-27 16:32:19 +0300

    JavaScript Code

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Kevin COOVI <https://github.com/kcoovi>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
